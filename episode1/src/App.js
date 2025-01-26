53b2f3b67468914c69b6acb1d71c736a3d16ec06
Xiangwei Zhang <139224333+WeiZhang0317@users.noreply.github.com>
2025-01-26 23:25:46 +1300

    course2

--- episode1/src/App.js
+++ episode1/src/App.js
@@ -1,12 +1,23 @@
-
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-     Hi React
+      <Job salary={90000} position="Senior SDE" company="Amazon" />
+      <Job salary={120000} position="Junior SDE" company="Google" />
+      <Job salary={100000} position="Project Manager" company="Netflix" />
     </div>
   );
 }
+
+const Job = (prompts) => {
+  return (
+    <div>
+      <h1>{prompts.salary}</h1>
+      <h1>{prompts.position}</h1>
+      <h1>{prompts.company}</h1>
+    </div>
+  );
+};
 
 export default App;
