f9e2c988ee2abf1050a8ac1bb92829f659fa35df
Xiangwei Zhang <139224333+WeiZhang0317@users.noreply.github.com>
2025-01-27 13:52:12 +1300

    三元运算

--- episode1/src/App.js
+++ episode1/src/App.js
@@ -1,23 +1,16 @@
+// Profile Component Example:
+// Name: John Doe
+// Age: 30
+// Profession: Software Engineer
+
 import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <Job salary={90000} position="Senior SDE" company="Amazon" />
-      <Job salary={120000} position="Junior SDE" company="Google" />
-      <Job salary={100000} position="Project Manager" company="Netflix" />
-    </div>
-  );
-}
+const age = 15;
 
-const Job = (prompts) => {
-  return (
-    <div>
-      <h1>{prompts.salary}</h1>
-      <h1>{prompts.position}</h1>
-      <h1>{prompts.company}</h1>
-    </div>
-  );
-};
+age >= 18 ? console.log("HEHE") : console.log("NOT HEHE");
+
+function App() {
+  return <div className="App"></div>;
+}
 
 export default App;
