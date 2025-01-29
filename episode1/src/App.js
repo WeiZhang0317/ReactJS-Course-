54a91eee646a5a483ae45f547cfd0779867691ff
Xiangwei Zhang <Wei.Zhang2@lincolnuni.ac.nz>
2025-01-29 17:36:32 +1300

    update show text

--- episode1/src/App.js
+++ episode1/src/App.js
@@ -3,16 +3,23 @@ import {useState} from "react";
 
 function App() {
 
-  const[inputValue,setInputValue]=useState("")
+  const[showText,setShowText]=useState(true);
 
   //inital value is the empty string
   //change inputValue by using setInputValue function
   
-  
+
   return(
   <div className="App">
-  <button>show/hide</button>
-  <h1>my name is o</h1>
+  <button 
+  onClick={()=>{setShowText(!showText)}}  
+  // setshowText 是一个函数，它用于 更新 showText 的值
+  // showText 是一个变量
+  >
+    show/hide</button>
+  {showText===true && <h1>my name is o</h1>}
+
+  {/* === 是 严格相等（strict equality） 运算符，它比较 值 和 数据类型，只有当 值和类型 都相同时，才会返回 true。 */}
   </div>
   );
   }
