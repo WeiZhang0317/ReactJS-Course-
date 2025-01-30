c0804503aa84b34941628e521ec08d98a5f24980
Xiangwei Zhang <Wei.Zhang2@lincolnuni.ac.nz>
2025-01-30 17:08:29 +1300

    exercise

--- episode1/src/App.js
+++ episode1/src/App.js
@@ -1,26 +1,20 @@
+
 import "./App.css";
-import {useState} from "react";
+import { useState } from "react";
 
 function App() {
+  const [count, setCount] = useState(0);
 
-  const[showText,setShowText]=useState(true);
 
-  //inital value is the empty string
-  //change inputValue by using setInputValue function
-  
+  return (
+    <div className="App">
+      <button onClick={()=>{setCount(count+1)}}>Increase</button>
+      <button onClick={()=>{setCount(count-1)}}>Decrease</button>
+      <button onClick={()=>{setCount(0)}}>Set to Zero</button>
 
-  return(
-  <div className="App">
-  <button 
-  onClick={()=>{setShowText(!showText)}}  
-  // setshowText 是一个函数，它用于 更新 showText 的值
-  // showText 是一个变量
-  >
-    show/hide</button>
-  {showText===true && <h1>my name is o</h1>}
-
-  {/* === 是 严格相等（strict equality） 运算符，它比较 值 和 数据类型，只有当 值和类型 都相同时，才会返回 true。 */}
-  </div>
+      {count}
+    </div>
   );
-  }
-  export default App;
+}
+
+export default App;
