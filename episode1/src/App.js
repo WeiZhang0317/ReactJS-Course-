ac4dbb35eda38edc78a5555e0cd5b7364b1e6be6
Xiangwei Zhang <Wei.Zhang2@lincolnuni.ac.nz>
2025-01-31 17:02:17 +1300

    update js

--- episode1/src/App.js
+++ episode1/src/App.js
@@ -1,18 +1,40 @@
-
 import "./App.css";
 import { useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const[todoList,setTodoList]=useState([]);
+  const[newTask,setNewTask]=useState("")
+  // create state to represent input value
+
+  const handleChange =(event) =>{
+   setNewTask(event.target.value);
+  };//这个函数会捕获用户在输入框中输入的内容，并更新状态 newTask，确保 React 知道用户输入了什么。
+
+  const addTask=()=>{
+     const newTodoList=[...todoList,newTask]
+     // ... means todolist+newtask 
+     setTodoList(newTodoList);
+    };
 
 
+
+  
   return (
     <div className="App">
-      <button onClick={()=>{setCount(count+1)}}>Increase</button>
-      <button onClick={()=>{setCount(count-1)}}>Decrease</button>
-      <button onClick={()=>{setCount(0)}}>Set to Zero</button>
 
-      {count}
+    <div className="addTask">
+    <input onChange={handleChange} />
+    {/*any changes pass in the input value  */}
+    <button onClick={addTask}>Add task</button>      
+    </div>
+
+    <div className="list">
+    {todoList.map((task)=>{
+     return<h1>{task}</h1>
+
+    })}
+
+    </div>
     </div>
   );
 }
