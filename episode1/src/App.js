4277da95aa864661b8be5790659bc42c0c7c7111
Xiangwei Zhang <Wei.Zhang2@lincolnuni.ac.nz>
2025-02-21 16:41:06 +1300

    finish course 6

--- episode1/src/App.js
+++ episode1/src/App.js
@@ -1,75 +1,23 @@
 import "./App.css";
 import { useState } from "react";
-import {Task} from "./Task.js"
+import { Text } from "./Text";
 
 function App() {
-  const [todoList, setTodoList] = useState([]);
-  const [newTask, setNewTask] = useState("");
-  const [textColor, setTextColor] = useState("red"); 
-
-
-  const handleChange = (event) => {
-    setNewTask(event.target.value);
-  };
-
-  const addTask = () => {
-    const task = {
-      id: todoList.length === 0 ? 1 : todoList[todoList.length - 1].id + 1,
-      taskName: newTask,
-      complete:false
-    };
-    setTodoList(task.taskName !== "" ? [...todoList, task] : todoList);
-  };
-
-  const deleteTask = (id) => {
-    setTodoList(todoList.filter((task) => task.id !== id));
-  };
-  const completeTask = (id) => {
-    setTodoList(
-      todoList.map((task) => {
-        if (task.id === id) {
-          return { ...task, completed: true };
-        } else {
-          return task;
-        }
-      })
-    );
-  };
-  
- const cancelCompleteTask =  (id) => {
-  setTodoList(
-    todoList.map((task) => {
-      if (task.id === id) {
-        return { ...task, completed: false };
-      } else {
-        return task;
-      }
-    })
-  );
-};
+  const [showText, setShowText] = useState(false);
 
   return (
     <div className="App">
-      <div className="addTask">
-        <input onChange={handleChange} />
-        <button onClick={addTask}> Add Task</button>
-      </div>
-      <div className="list">
-        {todoList.map((task) => {
-          return (
-            <Task
-            key={task.id}
-            task={task}
-            deleteTask={deleteTask}
-            completeTask={completeTask}
-            cancelCompleteTask={cancelCompleteTask}
-          />
-          
-          );
-        })}
-      </div>
+      <button
+        onClick={() => {
+          setShowText(!showText);
+        }}
+      >
+        Show Text
+      </button>
+
+      {showText && <Text />}
     </div>
   );
-
 }
+
 export default App;
